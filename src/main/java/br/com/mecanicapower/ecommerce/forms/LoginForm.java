af5b539f6a5c580ecc5a5c9bdbec338bc985dba2
YuriMCorrea <yurimcorrea@gmail.com>
2022-02-07 14:36:59 -0300

    switchong to stateless login, prepared

---
+++ src/main/java/br/com/mecanicapower/ecommerce/forms/LoginForm.java
@@ -0,0 +1,22 @@
+package br.com.mecanicapower.ecommerce.forms;
+
+public class LoginForm {
+	
+	private String email;
+	private String senha;
+	
+	// ----------------- GETTER'S & SETTER'S -------------------------
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}

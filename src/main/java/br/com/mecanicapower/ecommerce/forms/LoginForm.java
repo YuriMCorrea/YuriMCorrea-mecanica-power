17e01e6d78d5a58ff694864e43460bb2af9c6484
YuriMCorrea <yurimcorrea@gmail.com>
2022-02-07 16:24:44 -0300

    Authentication stateless with jwt fully implemented

--- src/main/java/br/com/mecanicapower/ecommerce/forms/LoginForm.java
+++ src/main/java/br/com/mecanicapower/ecommerce/forms/LoginForm.java
@@ -1,5 +1,7 @@
 package br.com.mecanicapower.ecommerce.forms;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
 public class LoginForm {
 	
 	private String email;
@@ -17,6 +19,9 @@ public class LoginForm {
 	}
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+	public UsernamePasswordAuthenticationToken converter() {
+		return new UsernamePasswordAuthenticationToken(email, senha);
 	}
 
 }
